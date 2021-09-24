b92ee2d8cd26b030cd86740ae98487d9ecd25779
not-alone <not-alone@yandex.ru>
2021-09-24 21:15:39 +0300

    just add a procedure

--- main.py
+++ main.py
@@ -2,6 +2,17 @@ from tkinter import *
 from tkinter.ttk import *
 from configparser import *
 
+def ReadConfigs():
+    config = ConfigParser()
+    config.read('conf.ini')
+    fGameIni = open(config['main']['GameUserSettingsIniPath'] + '\\' + 'Game.ini')
+    fGameUserSettingsIni = open(config['main']['GameUserSettingsIniPath'] + '\\' + 'GameUserSettings.ini')
+    gameIni.insert(1.0, fGameIni.read())
+    gameUserSettingsIni.insert(1.0, fGameUserSettingsIni.read())
+    fGameIni.close()
+    fGameUserSettingsIni.close()
+
+
 mainWindow = Tk()
 mainWindow.title("ArkControlClient")
 mainWindow.geometry("1024x768")
@@ -10,19 +21,13 @@ nb = Notebook(mainWindow)
 gameIni = Text(nb)
 gameUserSettingsIni = Text(nb)
 
+ReadConfigs()
 
 nb.add(gameIni, text='Game.ini')
 nb.add(gameUserSettingsIni, text='GameUserSettings.ini')
 nb.pack(fill='both', expand='yes')
 
-config=ConfigParser()
-config.read('conf.ini')
-fGameIni = open(config['main']['GameUserSettingsIniPath'] + '\\' + 'Game.ini')
-fGameUserSettingsIni = open(config['main']['GameUserSettingsIniPath'] + '\\' + 'GameUserSettings.ini')
 
-gameIni.insert(1.0, fGameIni.read())
-gameUserSettingsIni.insert(1.0, fGameUserSettingsIni.read())
 
-fGameIni.close()
-fGameUserSettingsIni.close()
 mainWindow.mainloop()
+
