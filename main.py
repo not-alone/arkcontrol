a6a89cc890e39f59459917d45e0d1f78acc44d91
not-alone <not-alone@yandex.ru>
2021-09-25 00:06:10 +0300

    Save file test

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
+import tkinter
 from tkinter import *
 from tkinter.ttk import *
 from configparser import *
 
-def ReadConfigs():
-    config = ConfigParser()
-    config.read('conf.ini')
-    fGameIni = open(config['main']['GameUserSettingsIniPath'] + '\\' + 'Game.ini')
-    fGameUserSettingsIni = open(config['main']['GameUserSettingsIniPath'] + '\\' + 'GameUserSettings.ini')
-    gameIni.insert(1.0, fGameIni.read())
-    gameUserSettingsIni.insert(1.0, fGameUserSettingsIni.read())
-    fGameIni.close()
-    fGameUserSettingsIni.close()
+
+def SaveConfigs():
+    file1=open(fGameIniPath, 'w')
+    file2=open(fGameUserSettingsIniPath, 'w')
+    file1.write(gameIni.get("1.0", END))
+    file2.write(gameUserSettingsIni.get("1.0", END))
+    file1.close()
+    file2.close()
+
 
 
 mainWindow = Tk()
@@ -20,14 +21,27 @@ mainWindow.geometry("1024x768")
 nb = Notebook(mainWindow)
 gameIni = Text(nb)
 gameUserSettingsIni = Text(nb)
-
-ReadConfigs()
-
 nb.add(gameIni, text='Game.ini')
 nb.add(gameUserSettingsIni, text='GameUserSettings.ini')
 nb.pack(fill='both', expand='yes')
 
+config = ConfigParser()
+config.read('conf.ini')
 
+fGameIniPath=config['main']['GameUserSettingsIniPath'] + '\\' + 'Game.ini'
+fGameUserSettingsIniPath=config['main']['GameUserSettingsIniPath'] + '\\' + 'GameUserSettings.ini'
+
+fGameIni = open(fGameIniPath)
+fGameUserSettingsIni = open(fGameUserSettingsIniPath)
+
+gameIni.insert(1.0, fGameIni.read())
+gameUserSettingsIni.insert(1.0, fGameUserSettingsIni.read())
+
+fGameIni.close()
+fGameUserSettingsIni.close()
+
+buttonSave = Button(mainWindow, text='Save', command=SaveConfigs)
+buttonSave.pack()
 
 mainWindow.mainloop()
 
