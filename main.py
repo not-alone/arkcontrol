62fde284d52d207e52d27dabb559031f2004a116
not-alone <not-alone@yandex.ru>
2021-09-25 13:28:32 +0300

    Addet scroolbar and menu save

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import tkinter
 from tkinter import *
 from tkinter.ttk import *
 from configparser import *
-
 
 def SaveConfigs():
     file1=open(fGameIniPath, 'w')
@@ -12,11 +10,13 @@ def SaveConfigs():
     file1.close()
     file2.close()
 
-
-
 mainWindow = Tk()
 mainWindow.title("ArkControlClient")
 mainWindow.geometry("1024x768")
+
+menubar=Menu(mainWindow)
+mainWindow.config(menu=menubar)
+menubar.add_command(label='SAVE', command=SaveConfigs)
 
 nb = Notebook(mainWindow)
 gameIni = Text(nb)
@@ -34,14 +34,16 @@ fGameUserSettingsIniPath=config['main']['GameUserSettingsIniPath'] + '\\' + 'Gam
 fGameIni = open(fGameIniPath)
 fGameUserSettingsIni = open(fGameUserSettingsIniPath)
 
-gameIni.insert(1.0, fGameIni.read())
-gameUserSettingsIni.insert(1.0, fGameUserSettingsIni.read())
+gameIni.insert(1.0, fGameIni.read().strip())
+gameUserSettingsIni.insert(1.0, fGameUserSettingsIni.read().strip())
 
 fGameIni.close()
 fGameUserSettingsIni.close()
 
-buttonSave = Button(mainWindow, text='Save', command=SaveConfigs)
-buttonSave.pack()
+sbGameIni=Scrollbar(gameIni, command=gameIni.yview)
+sbGameUserSettingsIni=Scrollbar(gameUserSettingsIni, command=gameUserSettingsIni.yview)
+sbGameIni.pack(side=RIGHT, fill=Y)
+sbGameUserSettingsIni.pack(side=RIGHT, fill=Y)
 
 mainWindow.mainloop()
 
